c1adc34df6c708f18d2e5f6b17a819cffdda4028
djnexxxy27 <nexxxy27@yahoo.co.uk>
2018-03-25 16:26:35 +0100

    my third one

---
+++ code.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+	window.addEventListener("scroll", function() {
+		var img = document.querySelector("header img");
+		img.style.width = (100+(window.scrollY/20))+"%";
+		img.style.left = (0-(window.scrollY/50))+"%";
+		img.style.opacity = 1-(1/(window.innerHight/window.scrollY));
+	});
+});
